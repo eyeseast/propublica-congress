499e178134866087525e49f5c4a4e48e2b1922d4
Gil Tolley <tolley.gil@gmail.com>
2018-02-12 14:13:00 -0500

    nominations.py should now be PEP8 compliant. Removed unneccessary whitespace and shortened a few lines by aligning variables on a new line.

--- congress/nominations.py
+++ congress/nominations.py
@@ -3,15 +3,17 @@ from .utils import CURRENT_CONGRESS
 
 
 class NominationsClient(Client):
-    
+
     def filter(self, type, congress=CURRENT_CONGRESS):
-        path = "{congress}/nominees/{type}.json".format(congress=congress, type=type)
+        path = "{congress}/nominees/{type}.json".format(congress=congress,
+                                                        type=type)
         return self.fetch(path)
-    
+
     def get(self, nominee, congress=CURRENT_CONGRESS):
-        path = "{congress}/nominees/{nominee}.json".format(congress=congress, nominee=nominee)
+        path = "{congress}/nominees/{nominee}.json".format(congress=congress,
+                                                           nominee=nominee)
         return self.fetch(path)
-    
+
     def by_state(self, state, congress=CURRENT_CONGRESS):
         path = "{congress}/nominees/state/{state}.json".format(
             congress=congress, state=state)
